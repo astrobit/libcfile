7eb8633b1eafda0b7d2e9ece1bff493098e00861
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-11-15 14:41:29 -0600

    do safety checking for mutex pointer

--- src/cfile_base.cpp
+++ src/cfile_base.cpp
@@ -12,11 +12,15 @@ std::mutex cfile_base::m_mtxSwap;
 bool cfile_base::flush(void) const
 {
 	bool bRet = false;
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	FILE * pFile = get_file_pointer();
-	if (pFile)
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		bRet = (std::fflush(pFile) == 0);
+		std::lock_guard<std::mutex> lock(*pMutex);
+		FILE * pFile = get_file_pointer();
+		if (pFile)
+		{
+			bRet = (std::fflush(pFile) == 0);
+		}
 	}
 	return bRet;
 }
@@ -54,8 +58,10 @@ bool cfile_base::open(const std::string &i_sFile, access_mode i_eAccess_Mode, da
 size_t cfile_base::read(void * o_lpBuffer, size_t i_nSize_Bytes) const
 {
 	size_t nRet = 0;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -67,8 +73,10 @@ size_t cfile_base::read(void * o_lpBuffer, size_t i_nSize_Bytes) const
 size_t cfile_base::write(void * o_lpBuffer, size_t i_nSize_Bytes) const
 {
 	size_t nRet = 0;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -80,8 +88,10 @@ size_t cfile_base::write(void * o_lpBuffer, size_t i_nSize_Bytes) const
 size_t cfile_base::printf(const std::string & i_sFormat, ...) const
 {
 	size_t nRet = 0;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -97,8 +107,10 @@ size_t cfile_base::printf(const std::string & i_sFormat, ...) const
 size_t cfile_base::scanf(const std::string & i_sFormat, ...) const
 {
 	size_t nRet = 0;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -115,8 +127,10 @@ std::string cfile_base::gets(size_t i_tSize_Bytes) const
 {
 	std::string sRet;
 	char nC;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -145,8 +159,10 @@ std::string cfile_base::gets_stripped(size_t i_tSize_Bytes) const
 {
 	std::string sRet;
 	char nC;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -172,8 +188,10 @@ std::string cfile_base::gets_stripped(size_t i_tSize_Bytes) const
 size_t cfile_base::puts(const std::string i_sString) const
 {
 	size_t tRet = 0;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -186,8 +204,10 @@ size_t cfile_base::puts(const std::string i_sString) const
 fpos_t cfile_base::getpos(void) const
 {
 	fpos_t cPos;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -200,8 +220,10 @@ fpos_t cfile_base::getpos(void) const
 bool cfile_base::setpos(fpos_t i_cPos) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -214,8 +236,10 @@ bool cfile_base::setpos(fpos_t i_cPos) const
 bool cfile_base::eof(void) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 		{
@@ -228,8 +252,10 @@ bool cfile_base::eof(void) const
 bool cfile_base::error(void) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 			bRet = (std::ferror(pFile) != 0);
@@ -242,10 +268,14 @@ bool cfile_base::rewind(size_t i_nDistance) const
 	bool bRet = false;
 	if  (i_nDistance == static_cast<size_t>(-1))
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-		FILE * pFile = get_file_pointer();
-		if (pFile != nullptr)
-			bRet = (std::fseek(pFile,0,SEEK_SET) == 0);
+		std::mutex * pMutex = get_mutex_pointer();
+		if (pMutex != nullptr)
+		{
+			std::lock_guard<std::mutex> lock(*pMutex);
+			FILE * pFile = get_file_pointer();
+			if (pFile != nullptr)
+				bRet = (std::fseek(pFile,0,SEEK_SET) == 0);
+		}
 	}
 	else
 	{
@@ -256,18 +286,24 @@ bool cfile_base::rewind(size_t i_nDistance) const
 			rewind(i_nDistance >> 1);
 			nDistance = -1 * static_cast<long int>(i_nDistance - (i_nDistance >> 1));
 		}
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-		FILE * pFile = get_file_pointer();
-		if (pFile != nullptr)
-			bRet = (std::fseek(pFile,nDistance,SEEK_CUR) == 0);
+		std::mutex * pMutex = get_mutex_pointer();
+		if (pMutex != nullptr)
+		{
+			std::lock_guard<std::mutex> lock(*pMutex);
+			FILE * pFile = get_file_pointer();
+			if (pFile != nullptr)
+				bRet = (std::fseek(pFile,nDistance,SEEK_CUR) == 0);
+		}
 	}
 	return bRet;
 }
 bool cfile_base::fast_forward(size_t i_nDistance) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 			bRet = (std::fseek(pFile,static_cast<long int>(i_nDistance),SEEK_CUR) == 0);
@@ -277,8 +313,10 @@ bool cfile_base::fast_forward(size_t i_nDistance) const
 bool cfile_base::seek(long int i_nDistance, int i_nOrigin) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 			bRet = (std::fseek(pFile,i_nDistance,i_nOrigin) == 0);
@@ -288,8 +326,10 @@ bool cfile_base::seek(long int i_nDistance, int i_nOrigin) const
 bool cfile_base::rewind_to_start(void) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 			bRet = (std::fseek(pFile,0,SEEK_SET) == 0);
@@ -299,8 +339,10 @@ bool cfile_base::rewind_to_start(void) const
 bool cfile_base::forward_to_end(void) const
 {
 	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
 	{
-		std::lock_guard<std::mutex> lock(*get_mutex_pointer());
+		std::lock_guard<std::mutex> lock(*pMutex);
 		FILE * pFile = get_file_pointer();
 		if (pFile != nullptr)
 			bRet = (std::fseek(pFile,0,SEEK_END) == 0);
@@ -309,44 +351,66 @@ bool cfile_base::forward_to_end(void) const
 }
 void cfile_base::clear_error(void) const
 {
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	FILE * pFile = get_file_pointer();
-	if (pFile != nullptr)
-		std::clearerr(pFile);
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
+	{
+		std::lock_guard<std::mutex> lock(*pMutex);
+		FILE * pFile = get_file_pointer();
+		if (pFile != nullptr)
+			std::clearerr(pFile);
+	}
 }
 
 char cfile_base::getc(void) const
 {
 	char nRet = EOF;
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	FILE * pFile = get_file_pointer();
-	if (pFile != nullptr)
-		nRet = std::fgetc(pFile);
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
+	{
+		std::lock_guard<std::mutex> lock(*pMutex);
+		FILE * pFile = get_file_pointer();
+		if (pFile != nullptr)
+			nRet = std::fgetc(pFile);
+	}
 	return nRet;
 }
 
 char cfile_base::putc(char i_nChar) const
 {
 	char nRet = EOF;
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	FILE * pFile = get_file_pointer();
-	if (pFile != nullptr)
-		nRet = std::fputc(i_nChar,pFile);
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
+	{
+		std::lock_guard<std::mutex> lock(*pMutex);
+		FILE * pFile = get_file_pointer();
+		if (pFile != nullptr)
+			nRet = std::fputc(i_nChar,pFile);
+	}
 	return nRet;
 }
 
 size_t cfile_base::tell(void) const
 {
 	size_t nRet = 0;
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	FILE * pFile = get_file_pointer();
-	if (pFile != nullptr)
-		nRet = static_cast<size_t>(std::ftell(pFile));
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
+	{
+		std::lock_guard<std::mutex> lock(*pMutex);
+		FILE * pFile = get_file_pointer();
+		if (pFile != nullptr)
+			nRet = static_cast<size_t>(std::ftell(pFile));
+	}
 	return nRet;
 }
 
 bool cfile_base::is_open(void) const
 {
-	std::lock_guard<std::mutex> lock(*get_mutex_pointer());
-	return (get_file_pointer() != nullptr);
+	bool bRet = false;
+	std::mutex * pMutex = get_mutex_pointer();
+	if (pMutex != nullptr)
+	{
+		std::lock_guard<std::mutex> lock(*pMutex);
+		bRet = (get_file_pointer() != nullptr);
+	}
+	return bRet;
 }
