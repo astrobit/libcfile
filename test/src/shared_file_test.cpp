bf2a36345d48caf778969854756d305ab63db5cb
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-12-01 22:41:30 -0600

    clean up test to only report success/failure; use workaround for bug in clang incorrectly warning of mismatched new/delete

--- test/src/shared_file_test.cpp
+++ test/src/shared_file_test.cpp
@@ -7,21 +7,21 @@ void test_shared_file(cfile::shared_file * pFile)
 	if (pFile != nullptr)
 	{
 		pFile->open("testfile.txt","wt");
-		printf("opened (write)\n");
+//		printf("opened (write)\n");
 		pFile->puts("this is a string\n");
-		printf("puts\n");
+//		printf("puts\n");
 		pFile->close();
-		printf("close (write)\n");
+//		printf("close (write)\n");
 		pFile->open("testfile.txt","rt");
-		printf("opened (read)\n");
+//		printf("opened (read)\n");
 		std::string sString = pFile->gets();
-		printf("gets %s\n",sString.c_str());
+//		printf("gets %s\n",sString.c_str());
 		if(strcmp(sString.c_str(),"this is a string\n") == 0)
 			printf("read/write match\n");
 		else
 			printf("read/write mismatch\n");
 		pFile->close();
-		printf("close (read)\n");
+//		printf("close (read)\n");
 	}
 }
 
@@ -29,38 +29,33 @@ int main(int i_iNum_Args, char * i_ppArgs[])
 {
 
 	cfile::shared_file cSF;
-	printf("file instantiated\n");
+//	printf("file instantiated\n");
 	test_shared_file(&cSF);
 
 	cfile::shared_file *pSF = new cfile::shared_file;
 	if (pSF != nullptr)
 	{
-		printf("SF file alocated\n");
+//		printf("SF file alocated\n");
 		test_shared_file(pSF);
 		delete pSF;
-		printf("SF file deleted\n");
+//		printf("SF file deleted\n");
 		pSF = nullptr;
 	}
 	else
 		printf("new SF failed\n");
 
-	pSF = new cfile::shared_file[3];
-	if (pSF != nullptr)
+	cfile::shared_file pSFA = new cfile::shared_file[3];
+	if (pSFA != nullptr)
 	{
-		printf("SF file array alocated\n");
+//		printf("SF file array alocated\n");
 		for (size_t nI = 0; nI < 3; nI++)
-			test_shared_file(&pSF[nI]);
-		delete [] pSF;
-		printf("SF file array deleted\n");
+			test_shared_file(&pSFA[nI]);
+		delete [] pSFA;
+//		printf("SF file array deleted\n");
 		pSF = nullptr;
 	}
 	else
 		printf("new SF array failed\n");
-
-
-
-	// try the bad way of allocating things
-	//pFile = new cfile::shared_file;
 
 	return 0;
 }
